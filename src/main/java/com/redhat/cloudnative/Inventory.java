4530608e0d6878bdd7e0ce8eed12360eaa66f1a8
Natale Vinto <nvinto@redhat.com>
2021-08-27 18:49:14 +0200

    Update to Quarkus 2 and Panache

--- src/main/java/com/redhat/cloudnative/Inventory.java
+++ src/main/java/com/redhat/cloudnative/Inventory.java
@@ -1,44 +1,22 @@
 package com.redhat.cloudnative;
 
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
 import javax.persistence.Column;
-import java.io.Serializable;
 
 @Entity 
 @Table(name = "INVENTORY") 
-public class Inventory implements Serializable {
-
-    private static final long serialVersionUID = 1L;
-
-    @Id 
-    private String itemId;
+public class Inventory  extends PanacheEntity{
 
     @Column
-    private int quantity;
+    public int quantity;
 
-    public Inventory() {
-    }
-
-    public String getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(String itemId) {
-        this.itemId = itemId;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
 
     @Override
     public String toString() {
-        return "Inventory [itemId='" + itemId + '\'' + ", quantity=" + quantity + ']';
+        return "Inventory [Id='" + id + '\'' + ", quantity=" + quantity + ']';
     }
 }
